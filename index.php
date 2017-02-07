b563042ec3e89d6083370aea1de51dfc77c425ae
fhuszti <f.huszti@gmail.com>
2017-02-07 15:26:55 +0100

    Changed jQuery source from CDN to local

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
         </div>
 
         <!-- Loading jQuery -->
-        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+        <script src="assets/js/jquery-3.1.1.min.js"></script>
 
         <!-- Loading BootStrap JavaScript -->
         <script src="bootstrap/js/bootstrap.min.js"></script>
