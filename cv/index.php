6fd7cd57894b96d3327446a13bb9be3d6e64e7d6
fhuszti <f.huszti@gmail.com>
2017-02-10 15:10:27 +0100

    Resume category page is now up and running

---
+++ cv/index.php
@@ -0,0 +1,55 @@
+<?php require("../php-config/langSelect.php"); ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $lang['META_TITLE']; ?></title>
+
+        <meta name="Description" content="<?php echo $lang['META_DESC']; ?>" />
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="../assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="../assets/css/header.css">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body role="document">
+        <?php require("../header.php"); ?>
+
+        <div id="wrapper" class="container-fluid" role="main">
+            <section class="row">
+                <div class="col-sm-6 col-md-4 hiddenOverflow">
+                    <div id="ctaPart1" class="col-xs-12 ctaPart">
+                        <h2 class="ctaPartTitle"><?php echo $lang['CV_SIMPLE']; ?></h2>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 hiddenOverflow">
+                    <div id="ctaPart2" class="col-xs-12 ctaPart">
+                        <h2 class="ctaPartTitle"><?php echo $lang['CV_INTERACTIVE']; ?></h2>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4 hiddenOverflow">
+                    <div id="ctaPart3" class="col-xs-12 ctaPart">
+                        <h2 class="ctaPartTitle"><?php echo $lang['HOME']; ?></h2>
+                    </div>
+                </div>
+            </section>
+        </div>
+
+        <!-- Loading jQuery -->
+        <script src="../assets/js/jquery-3.1.1.min.js"></script>
+
+        <!-- Loading BootStrap JavaScript -->
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ cv/languages/lang.en.php
@@ -0,0 +1,35 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+// NAVBAR
+$lang['MENU_PRINT_IMG_ALT'] = "Print the document";
+$lang['MENU_PRINT_DESC'] = "Print";
+
+$lang['MENU_HOME_LINK_TITLE'] = "Go back to the homepage";
+$lang['MENU_HOME'] = 'Home';
+$lang['MENU_CV_SIMPLE_LINK_TITLE'] = "My resume in its simple version";
+$lang['MENU_CV_SIMPLE'] = 'Resume';
+$lang['MENU_CV_INTERACTIVE_LINK_TITLE'] = "My resume in its interactive version";
+$lang['MENU_CV_INTERACTIVE'] = 'Interactive resume';
+$lang['MENU_HOMEWORK_LINK_TITLE'] = "Link to all the projects I've done";
+$lang['MENU_HOMEWORK'] = 'Homework';
+
+//CONTENT
+$lang['META_DESC'] = 'All different versions of my resume : interactive, basic with colors and black&white + printable.';
+$lang['META_TITLE'] = 'Resume - François Huszti, fullstack dev';
+
+$lang['CV_SIMPLE'] = "Resume - Simple version";
+$lang['CV_SIMPLE_LINK_TITLE'] = 'Access the simple version of my resume';
+$lang['CV_SIMPLE_PRINTABLE'] = 'To get to the black & white, printable version, <strong onclick="frames[\'frame\'].print()"><em>click  here</em></strong>.';
+
+$lang['CV_INTERACTIVE'] = "Resume - Interactive version";
+$lang['CV_INTERACTIVE_LINK_TITLE'] = 'Access the interactive version of my resume';
+
+$lang['HOME'] = 'Homepage';
+$lang['HOME_LINK_TITLE'] = 'Go back to the homepage';
