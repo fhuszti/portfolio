fb72cde34f4dace6e427d42b904130db580cb19c
fhuszti <f.huszti@gmail.com>
2017-12-01 16:17:21 +0100

    Moved assets from web/bundles/app/ to web/ and updated the code

--- web/js/5977eeb_part_3_ExperiencePlayerManager_11.js
+++
@@ -1,7 +0,0 @@
-var ExperiencePlayerManager = {
-
-	//create player
-	createPlayer: function() {
-		gameVariables.currentState.player = gameVariables.currentState.game.add.sprite(30, 60, 'textureAtlas', 'ninja1');
-	}
-};
