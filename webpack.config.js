cc500f1dc372b4adba73e0787294751455fb329a
fhuszti <f.huszti@gmail.com>
2017-12-08 23:13:39 +0100

    Layout and home templates now run without assetic

--- webpack.config.js
+++ webpack.config.js
@@ -16,8 +16,20 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    .addEntry('js/formHandler', './assets/js/formHandler.js')
+    .addStyleEntry('css/home', './assets/css/home.css')
+    
+    // this creates a 'vendor.js' file with jquery and the bootstrap JS module
+    .createSharedEntry('vendor', [
+        'jquery',
+        'bootstrap',
+
+        // you can also extract CSS - this will create a 'vendor.css' file
+        'bootstrap-sass/assets/stylesheets/_bootstrap.scss',
+        './assets/css/main.css',
+        './assets/css/header.css',
+        './assets/css/footer.css'
+    ])
 
     // use Sass/SCSS files pre-processor
     .enableSassLoader()
