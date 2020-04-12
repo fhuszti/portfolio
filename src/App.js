7a1d016e8906c783c4d5f645c2866d60c72fc72e
François Huszti <f.huszti@gmail.com>
2020-04-12 18:19:11 +0200

    First setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './App.scss';
 
 const App = () => {
-  return (
-    <div>App</div>
-  );
+	return (
+		<main>
+			<section className="hero">
+				Hero
+			</section>
+			<section className="skills-projects">
+				<section className="skills">
+					Skills
+				</section>
+				<section className="projects">
+					Projects
+				</section>
+			</section>
+			<section className="contact">
+				Contact
+			</section>
+		</main>
+	);
 };
 
 export default App;
