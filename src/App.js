f5ccd292960d1d166e2dffc97b574ff3fb1c9163
François Huszti <f.huszti@gmail.com>
2020-05-21 20:07:31 +0200

    Hero images and text are up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.scss';
+import './assets/scss/index.scss';
+import { ReactComponent as CodeSVG } from "./assets/svg/code.svg";
+import myAvatar from "./assets/images/myAvatar.png";
 
 const App = () => {
 	return (
-		<main>
+		<main className="main-wrapper">
 			<section className="hero">
-				Hero
+				<section className="hero-display">
+					<h1 className="hero-title">
+						Je suis François Huszti, <span className="span-job">développeur web fullstack,</span> et j'aimerai apporter ma pierre à votre édifice
+					</h1>
+				</section>
+				<aside className="hero-images">
+					<CodeSVG className="code-svg"/>
+					<img className="avatar-svg" src={myAvatar} alt="Mon avatar" />
+				</aside>
 			</section>
+			<div className="divider-hero-skills"/>
 			<section className="skills-projects">
 				<section className="skills">
 					Skills
