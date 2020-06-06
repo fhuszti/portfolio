89d1651d72a44f6dd14267cb580a9cc2dffd46d8
François Huszti <f.huszti@gmail.com>
2020-06-06 17:00:46 +0200

    BarChart now takes an array of data to display

--- components/main/Skills.js
+++ components/main/Skills.js
@@ -2,6 +2,20 @@ import React from 'react';
 import styles from '../../scss/main/Skills.module.scss';
 import BarChart from "../sub/bar_chart/BarChart";
 
+const data = [
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+	["HTML5", { width: '90%', backgroundColor: 'blue' }],
+];
+
 const Skills = () => {
 	return (
 		<section id="skills" className={styles.skillsWrapper}>
--- components/sub/bar_chart/BarChart.js
+++ components/sub/bar_chart/BarChart.js
@@ -3,7 +3,13 @@ import styles from '../../../scss/sub/bar_chart/BarChart.module.scss';
 import Level from "./Level";
 import Bar from "./Bar";
 
-const BarChart = () => {
+const BarChart = ({ data }) => {
+	const labels = data.map(elmt => elmt[0]);
+	const bars = data.map(elmt => elmt[1]);
+
+	const renderLabels = () => labels.map((label, i) => <p key={label + i} className={styles.label}>{label}</p>);
+	const renderBars = () => bars.map((css, i) => <Bar key={i} css={css}/>);
+
 	return (
 		<section className={styles.barchartWrapper}>
 			<section className={styles.labels}>
@@ -27,5 +33,9 @@ const BarChart = () => {
 		</section>
 	);
 };
+
+BarChart.defaultProp = {
+	data: [],
+}
 
 export default BarChart;
