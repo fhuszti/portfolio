dbef3cc4e6962a20f9933fdb2389b6709718c384
François Huszti <f.huszti@gmail.com>
2020-06-07 17:59:37 +0200

    Added font for titles

--- components/main/Skills.js
+++ components/main/Skills.js
@@ -3,22 +3,22 @@ import styles from '../../scss/main/Skills.module.scss';
 import BarChart from "../sub/bar_chart/BarChart";
 
 const data = [
-	["HTML5", { width: '90%', backgroundColor: '#004800' }],
-	["CSS3", { width: '90%', backgroundColor: '#145714' }],
-	["JavaScript (incl. ES6)", { width: '82%', backgroundColor: '#286628' }],
-	["PHP (5,7)", { width: '83%', backgroundColor: '#3a733a' }],
-	["MySQL", { width: '45%', backgroundColor: '#518451' }],
+	["HTML5", { width: '90%', backgroundColor: '#ffce54' }],
+	["CSS3", { width: '90%', backgroundColor: '#ac92ec' }],
+	["JavaScript (incl. ES6)", { width: '82%', backgroundColor: '#5d9cec' }],
+	["PHP (5,7)", { width: '83%', backgroundColor: '#ed5565' }],
+	["MySQL", { width: '45%', backgroundColor: '#a0d468' }],
 	["-----", { width: '0%', backgroundColor: 'rgba(0,0,0,0)' }],
-	["React", { width: '78%', backgroundColor: '#6b966b' }],
-	["React-Redux", { width: '57%', backgroundColor: '#84a884' }],
-	["Redux-Saga", { width: '30%', backgroundColor: '#99b799' }],
-	["Phaser.js", { width: '60%', backgroundColor: '#a9c2a9' }],
-	["Next.js", { width: '30%', backgroundColor: '#bcd0bc' }],
-	["Express.js", { width: '30%', backgroundColor: '#cfddcf' }],
-	["Twig", { width: '75%', backgroundColor: '#dde7dd' }],
+	["React", { width: '78%', backgroundColor: '#4fc1e9' }],
+	["React-Redux", { width: '57%', backgroundColor: '#ffce54' }],
+	["Redux-Saga", { width: '30%', backgroundColor: '#ac92ec' }],
+	["Phaser.js", { width: '60%', backgroundColor: '#5d9cec' }],
+	["Next.js", { width: '30%', backgroundColor: '#ed5565' }],
+	["Express.js", { width: '30%', backgroundColor: '#a0d468' }],
+	["Twig", { width: '75%', backgroundColor: '#4fc1e9' }],
 	["-----", { width: '0%', backgroundColor: 'rgba(0,0,0,0)' }],
-	["Symfony", { width: '85%', backgroundColor: '#ebf1eb' }],
-	["Doctrine", { width: '72%', backgroundColor: '#ffffff' }],
+	["Symfony", { width: '85%', backgroundColor: '#ffce54' }],
+	["Doctrine", { width: '72%', backgroundColor: '#ac92ec' }],
 ];
 
 const Skills = () => {
